d8c53aecb5d00385dafc872899e644efa2648ff0
yukinorei <yokomizoyuuki@yokomizoyuukinoMacBook-Pro.local>
2018-03-13 13:18:49 +0900

    DBcrate
    DB作成および
    プロジェクトテーブル作成

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,7 @@ module Wantedly22b
       g.helper          false
       g.javascripts     false
       g.test_framework  false
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
