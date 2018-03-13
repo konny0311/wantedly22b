92506ad6cd8236dbf8d257dc9c114f3f2992fbec
yukinorei <yokomizoyuuki@yokomizoyuukinoMacBook-Pro.local>
2018-03-13 13:00:20 +0900

    add_migrate
    Projectsのマイグレーションファイル編集

--- db/migrate/20180313035049_create_projects.rb
+++ db/migrate/20180313035049_create_projects.rb
@@ -1,7 +1,11 @@
 class CreateProjects < ActiveRecord::Migration[5.0]
   def change
     create_table :projects do |t|
-
+      t.text :title, null: false, index: true
+      t.text :content, null: false
+      t.string :job_title, null: false
+      t.integer :job_type, null: false, index: true
+      t.references :company, foreign_key: true,
       t.timestamps
     end
   end
