8bccb77386a3297c8c2fdde663303186437b9e8c
kono <konotorie@gmail.com>
2018-03-14 19:20:12 +0900

    Put class in a view file

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,4 +2,8 @@ class ProjectsController < ApplicationController
 
   def index
   end
+
+  def show
+    @project = Project.find(params[:id])
+    # @employments = Employment.where('id = project.company_id').limit(4)
 end
