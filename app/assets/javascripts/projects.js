fe3c432ae7f8f7caf78658b76f9cec22ed053631
kono <konotorie@gmail.com>
2018-03-23 19:54:10 +0900

    Put smog effect on buttons and more
    And action (あとで見る→保存しました) was mounted on check—later__btn with
    fontawesome

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -1,5 +1,8 @@
 $(function() {
   $('.contents--right__interests--top--centered__check--later__btn').on('click', function(e){
-    this.innerHTML='<div class="contents--right__interests--top--centered__check--later__btn">保存しました</div>';
+    this.innerHTML= "<span class='check'>保存しました</span>"
+  })
+  $('.later__btn--bottom').on('click', function(e){
+    this.innerHTML= "<span class='check'>保存しました</span>"
   })
 })
