bde5637845f7a7237a76574c284bcf403b6e713c
kono <konotorie@gmail.com>
2018-03-23 14:01:47 +0900

    Set js on 'あとで見る'btn
    Still needs to remove the parent element.

---
+++ app/assets/javascripts/projects.js
@@ -0,0 +1,5 @@
+$(function() {
+  $('.contents--right__interests--top--centered__check--later__btn').on('click', function(e){
+    this.innerHTML='<div class="contents--right__interests--top--centered__check--later__btn">保存しました</div>';
+  })
+})
