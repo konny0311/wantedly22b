09d00a668f23d85df853f7bba716066cc68017c9
kono <konotorie@gmail.com>
2018-03-26 19:47:40 +0900

    Set linkage of buttons of JS

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -1,18 +1,32 @@
 $(function() {
-  $('.contents--right__interests--top--centered__check--later__btn').on('click', function(e){
-    this.innerHTML= "<span class='check'>保存しました</span>"
+  // 保存ボタン
+  var c1 = $('.contents--right__interests--top--centered__check--later__btn')[0];
+  var c2 = $('.later__btn--bottom')[0];
+  $([c1, c2]).on('click', function(){
+    c1.innerHTML= "<span class='check'>保存しました</span>"
+    c2.innerHTML= "<span class='check'>保存しました</span>"
   })
-  $('.later__btn--bottom').on('click', function(e){
-    this.innerHTML= "<span class='check'>保存しました</span>"
+  // エントリーボタン
+  var e1 = $('.contents--right__interests--top--centered__entry__btn')[0];
+  var e2 = $('.entry__btn--bottom')[0];
+  $([e1, e2]).on('click', function(){
+    e1.innerHTML= "<span class='entry'>エントリーしました</span>"
+    e2.innerHTML= "<span class='entry'>エントリーしました</span>"
   })
-
+  // 応援ボタン
+  var s1 = $('.contents--right__interests--bottom__cheer__btn')[0];
+  var s2 = $('.support__btn--bottom')[0];
+  $([s1, s2]).on('click', function(){
+    s1.innerHTML= "<span class='support'>応援しました</span>"
+    s2.innerHTML= "<span class='support'>応援しました</span>"
+  })
+  // modal_window
   $('.entry__detail').on('click', function(){
-    $('#overlay, #modal_window--close, #modal_window__projects__entry').fadeIn();
+    $('#overlay, #modal_window__projects__entry').fadeIn();
   })
-  $('#overlay, #modal_window--close').on('click', function(){
-    $('#overlay, #modal_window--close, #modal_window__projects__entry').fadeOut();
+  $('#overlay').on('click', function(){
+    $('#overlay, #modal_window__projects__entry').fadeOut();
   })
-
   $('.support__detail').on('click', function(){
     $('#overlay,  #modal_window__projects__support').fadeIn();
   })
