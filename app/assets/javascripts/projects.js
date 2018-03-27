706ec5254c0b22b5e0ea41fbf162f3676dbad47b
kono <konotorie@gmail.com>
2018-03-27 15:13:15 +0900

    amendment
    change name of variables/file names and id names in
    _projects-detail-entry.html.haml and create mixin/_normal-font.scss.
    Projects controller is also amended using relation between company and
    project

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -1,32 +1,31 @@
 $(function() {
   // 保存ボタン
-  var c1 = $('.contents--right__interests--top--centered__check--later__btn')[0];
-  var c2 = $('.later__btn--bottom')[0];
+  var checkBtnRight = $('.contents--right__interests--top--centered__check--later__btn')[0];
+  var checkBtnBottom = $('.later__btn--bottom')[0];
   var check = "<span class='check'>保存しました</span>";
-  $([c1, c2]).on('click', function(){
-    // console.log(span-check)
-    c1.innerHTML= check;
-    c2.innerHTML= check;
+  $([checkBtnRight, checkBtnBottom]).on('click', function(){
+    checkBtnRight.innerHTML= check;
+    checkBtnBottom.innerHTML= check;
   })
   // エントリーボタン
-  var e1 = $('.contents--right__interests--top--centered__entry__btn')[0];
-  var e2 = $('.entry__btn--bottom')[0];
+  var entryBtnRight = $('.contents--right__interests--top--centered__entry__btn')[0];
+  var entryBtnBottom = $('.entry__btn--bottom')[0];
   var entry = "<span class='entry'>エントリーしました</span>";
-  $([e1, e2]).on('click', function(){
-    e1.innerHTML= entry;
-    e2.innerHTML= entry;
+  $([entryBtnRight, entryBtnBottom]).on('click', function(){
+    entryBtnRight.innerHTML= entry;
+    entryBtnBottom.innerHTML= entry;
   })
   // 応援ボタン
-  var s1 = $('.contents--right__interests--bottom__cheer__btn')[0];
-  var s2 = $('.support__btn--bottom')[0];
+  var supportBtnRight = $('.contents--right__interests--bottom__cheer__btn')[0];
+  var supportBtnBottom = $('.support__btn--bottom')[0];
   var support = "<span class='support'>応援しました</span>";
-  $([s1, s2]).on('click', function(){
-    s1.innerHTML= support;
-    s2.innerHTML= support;
+  $([supportBtnRight, supportBtnBottom]).on('click', function(){
+    supportBtnRight.innerHTML= support;
+    supportBtnBottom.innerHTML= support;
   })
   // modal_window
-  var modal_window_entry = $('#overlay, #modal_window__projects__entry');
-  var modal_window_support = $('#overlay,  #modal_window__projects__support');
+  var modal_window_entry = $('#overlay, #modal-window__projects__entry');
+  var modal_window_support = $('#overlay,  #modal-window__projects__support');
   $('.entry__detail').on('click', function(){
     modal_window_entry.fadeIn();
   })
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -6,7 +6,7 @@ class ProjectsController < ApplicationController
   def show
     @project = Project.find(params[:id])
     @company = @project.company
-    @projects = Project.where("company_id = #{@company.id} && id != #{params[:id]}").limit(4)
+    @projects = @company.projects.where("id != #{params[:id]}").limit(4)
     @feeds = Feed.where("company_id = #{@company.id}").limit(4)
     @address = CGI.escape("#{@company.address}")
     # @employments = Employment.where('id = project.company_id').limit(4)
