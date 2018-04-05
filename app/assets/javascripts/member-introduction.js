632b8ca91a688f9c462db01434c141abf618266f
kono <konotorie@gmail.com>
2018-04-05 18:21:06 +0900

    Get a correct catch copy related to user_id of a hovered member

--- app/assets/javascripts/member-introduction.js
+++ app/assets/javascripts/member-introduction.js
@@ -3,10 +3,14 @@ $(function() {
   var member = $('.contents--left__company__members__image__container');
   var sentence = $('.contents--left__company__members__introduction');
   member.hover(function() {
-    console.log(this)
+    // console.log(this)
     var id = this.dataset.user_id;
-    console.log(id)
+    // console.log(id)
+    var catchcopy = $('.catchcopy')[id-1];
+    console.log(catchcopy);
     // 自己紹介文はli(display:none)として記載
     // 写真hoverされたもののidを入手し、id一致するもののみli(display:list-item)化
+
+
   })
 })
