6cabeec7f0a4aaea6406f023497585ee25a5c230
kono <konotorie@gmail.com>
2018-04-05 17:58:18 +0900

    Get user_id when hovering

--- app/assets/javascripts/member-introduction.js
+++ app/assets/javascripts/member-introduction.js
@@ -4,6 +4,8 @@ $(function() {
   var sentence = $('.contents--left__company__members__introduction');
   member.hover(function() {
     console.log(this)
+    var id = this.dataset.user_id;
+    console.log(id)
     // thisからuser_id取得し、自己紹介表示に利用したい
   })
 })
