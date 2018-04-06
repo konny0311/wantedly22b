e6af11e0bdf3ac799f6c7ff8a8b51aa7c6fd5dfe
kono <konotorie@gmail.com>
2018-04-06 16:48:56 +0900

    Member introduction sentence is deleted when mouseleaving

--- app/assets/javascripts/member-introduction.js
+++ app/assets/javascripts/member-introduction.js
@@ -2,16 +2,20 @@ $(function() {
   // 求人画面にてメンバー画像の上にカーソルを乗せると自己紹介が現れる
   var member = $('.contents--left__company__members__image__container');
   var sentence = $('.contents--left__company__members__introduction');
-  member.hover(function() {
+  member.on({'mouseenter': function() {
     // console.log(this)
     var id = this.dataset.user_id;
     // console.log(id)
     var catchcopy = $('.catchcopy')[id-1];
-    console.log(catchcopy);
+    // console.log(catchcopy);
     catchcopy.className = "catchcopy--shown"
     // 自己紹介文はli(display:none)として記載
     // 写真hoverされたもののidを入手し、id一致するもののみli(display:list-item)化
-
-
+    },
+    'mouseleave': function() {
+    var catchcopy = $('.catchcopy--shown')[0];
+    console.log(catchcopy);
+    catchcopy.className = "catchcopy"
+    }
   })
 })
