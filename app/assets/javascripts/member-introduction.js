b13182a84275881da407b021b488aa16c4c01d1f
kono <konotorie@gmail.com>
2018-04-06 17:14:07 +0900

    Delete unnecessary codes such as comments

--- app/assets/javascripts/member-introduction.js
+++ app/assets/javascripts/member-introduction.js
@@ -2,12 +2,10 @@ $(function() {
   // 求人画面にてメンバー画像の上にカーソルを乗せると自己紹介が現れる
   var member = $('.contents--left__company__members__image__container');
   var sentence = $('.contents--left__company__members__introduction');
-  member.on({'mouseenter': function() {
-    // console.log(this)
+  member.on({
+    'mouseenter': function() {
     var id = this.dataset.user_id;
-    // console.log(id)
     var catchcopy = $('.catchcopy')[id-1];
-    // console.log(catchcopy);
     catchcopy.className = "catchcopy--shown"
     // 自己紹介文はli(display:none)として記載
     // 写真hoverされたもののidを入手し、id一致するもののみli(display:list-item)化
